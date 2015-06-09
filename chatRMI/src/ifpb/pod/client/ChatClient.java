98558831104ee6410902ebf52fb4ddb69a8c64da
mandalp <amandalops00@gmail.com>
2015-06-09 19:52:25 -0300

    Criação do Switch de opcoes no Cliente

--- chatRMI/src/ifpb/pod/client/ChatClient.java
+++ chatRMI/src/ifpb/pod/client/ChatClient.java
@@ -1,20 +1,35 @@
 package ifpb.pod.client;
 
 import ifpb.pod.server.ChatServerIF;
+
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
 
 public class ChatClient extends UnicastRemoteObject implements ChatClientIF {
 	private static final long serialVersionUID = 1L;
 	private String name = null;
+	private static Scanner keyboard = new Scanner(System.in);
+	
+	public static ChatServerIF chatServer;
 	
 
 	protected ChatClient(String name) throws RemoteException {
 		this.name = name;
 		
+		try {
+			String chatServerURL = "rmi://10.0.4.70/RMIChatServer";
+			chatServer = (ChatServerIF) Naming.lookup(chatServerURL);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public void retrieveMessage(String message) throws RemoteException {
@@ -31,15 +46,32 @@ public class ChatClient extends UnicastRemoteObject implements ChatClientIF {
 		this.name = name;
 	}
 
-	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
-		String chatServerURL = "rmi://10.0.4.70/RMIChatServer";
-		ChatServerIF chatServer = (ChatServerIF) Naming.lookup(chatServerURL);
-		chatServer.registerChatClient(new ChatClient("Amanda"));
-		chatServer.broadcastMessage("Enviei");
-		while(true){
-			
+	public static void main(String[] args) {
+		
+		try{
+			System.out.println("Digite o nome do usuário:");
+        	ChatClient cliente = new ChatClient(keyboard.nextLine());
+        	while(true){
+    			chatServer.menu(cliente);
+    			String cmd = keyboard.nextLine();
+    			switch (cmd){
+    				case "1":
+    					chatServer.broadcastMessage(message);
+    					break;
+    					
+    			}
+    			
+    		}
+		}catch (RemoteException ex) {
 		}
 		
-
+		
+		
+		
+		
+	
+	
 	}
+
+	
 }
