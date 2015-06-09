3086d67cb36fcf1df7a83f407e5b01124f7c70a4
Manuela Andrade <manuelandrade_@hotmail.com>
2015-06-08 21:32:57 -0300

    chat basico

---
+++ chatRMI/src/ifpb/pod/client/ChatClientDriver.java
@@ -0,0 +1,19 @@
+package ifpb.pod.client;
+
+import ifpb.pod.server.ChatServerIF;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class ChatClientDriver {
+
+	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
+		String chatServerURL = "rmi://localhost/RMIChatServer";
+		ChatServerIF chatServer = (ChatServerIF) Naming.lookup(chatServerURL);
+		new Thread(new ChatClient(args[0], chatServer)).start();
+
+	}
+	
+}
